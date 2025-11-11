8a873e1fc4a90717b1e988b20eaf2becd1ff001f
parhamoq <parhamoq@gmail.com>
2025-11-11 19:09:01 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
-// Supabase Configuration - این کلیدها برای تمیزی و مدیریت بهتر از فایل اصلی جدا شده‌اند.
-// توجه: این کلیدها هنوز هم در مرورگر قابل مشاهده هستند، اما امنیت توسط Row Level Security در Supabase تأمین می‌شود.
-
-// تنظیمات Supabase شما (مقادیر واقعی)
-export const supabaseUrl = 'https://jljudmnngrepmhgzwhhn.supabase.co'; 
+export const supabaseUrl = 'https://jljudmnngrepmhgzwhhn.supabase.co';
 export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsanVkbW5uZ3JlcG1oZ3p3aGhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2OTg3MDAsImV4cCI6MjA3ODI3NDcwMH0.ajjY57gb92PwiOpzoRs6HFRfeBn9G-wSVbt9-lmNCU0'; 
-
-// متغیرهای Placeholder برای اعتبارسنجی داخلی برنامه
-export const EXAMPLE_URL = 'https://your-project.supabase.co'; 
-export const EXAMPLE_KEY = 'YOUR_ANON_KEY_HERE';
+export const ESP_NAME = 'SuperSmartOven';
+export const WIFI_PROV_SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c0c6c47c8a99';
+export const SSID_CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
+export const PASS_CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a9';
