af218d3d5b6fc28fd7fad5cd79fac26eec058083
parhamoq <parhamoq@gmail.com>
2026-06-29 20:11:57 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const supabaseUrl = 'https://jljudmnngrepmhgzwhhn.supabase.co';
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsanVkbW5uZ3JlcG1oZ3p3aGhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2OTg3MDAsImV4cCI6MjA3ODI3NDcwMH0.ajjY57gb92PwiOpzoRs6HFRfeBn9G-wSVbt9-lmNCU0'; 
+export const supabaseUrl = 'https://wurdazvtovxjogurmhbe.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1cmRhenZ0b3Z4am9ndXJtaGJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzY4NTgsImV4cCI6MjA5ODMxMjg1OH0.hjzfvLIbF4QGOOfRemkkfYb9RG_N5p7moFyJ8MR23ao'; 
 export const ESP_NAME = 'SuperSmartOven';
 export const WIFI_PROV_SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c0c6c47c8a99';
 export const SSID_CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
